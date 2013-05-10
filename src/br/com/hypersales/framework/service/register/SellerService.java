5d13188d3d25b5f6127a71694b20b7a86454640e
Anderson Figueredo Gomes <gomes.ad@gmail.com>
2013-05-10 17:53:45 -0300

    Método getList busca dados pelo WS Protheus

--- src/br/com/hypersales/framework/service/register/SellerService.java
+++ src/br/com/hypersales/framework/service/register/SellerService.java
@@ -1,5 +1,16 @@
 package br.com.hypersales.framework.service.register;
 
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.xml.sax.SAXException;
+
+import br.com.hypersales.framework.dao.Wsp;
+import br.com.hypersales.framework.dao.protheus.ARRAYOFSTRING;
+import br.com.hypersales.framework.dao.protheus.STRUCTRETURN;
+import br.com.hypersales.framework.model.register.Payment;
 import br.com.hypersales.framework.model.register.Seller;
 import br.com.hypersales.framework.presentation.JsonResult;
 import br.com.hypersales.framework.presentation.JsonResultList;
@@ -10,6 +21,41 @@ public class SellerService {
 	public JsonResultList<Seller> getAllSellers() {
 		JsonResultList<Seller> result = new JsonResultList<Seller>();
 		
+		
+		Wsp daoWs = new Wsp();
+		try {
+			STRUCTRETURN retorno = daoWs.execute("Seller_getList", null);
+			if (retorno.getRESPONSEMESSAGE().equals("OK")) {
+				ARRAYOFSTRING item = retorno.getSALESORDERID();
+				for(String record : item.getSTRING()) {
+					record = record.substring(1);
+					record = record.substring(0, record.length() -2);
+
+					String[] keyValueRecord = record.split("\",\"");
+
+					Seller pRet = new Seller();
+					pRet.setShortName("");
+
+					for(String kv : keyValueRecord) {
+						String[] keyValue = kv.split("\":\"");
+						if (keyValue[0].toUpperCase().equals("SELLERID")) {
+							pRet.setId(keyValue[1]);
+						}
+						if (keyValue[0].toUpperCase().equals("SELLERNAME")) {
+							pRet.setName(keyValue[1]);
+						}
+					}
+					result.add(pRet);
+				}
+			}
+		} catch (XPathExpressionException | ParserConfigurationException
+				| SAXException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		/*
 		result.setResponseId(RequestStatus.SUCCESS.statusCode());
 		result.setResponseMessage(RequestStatus.SUCCESS.toString());
 
@@ -19,7 +65,7 @@ public class SellerService {
 
 			result.add(s);
 		}
-
+		*/
 		return result;
 	}
 
