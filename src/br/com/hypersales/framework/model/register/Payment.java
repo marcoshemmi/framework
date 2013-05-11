efd0af0b432a3ee43fc1f6f01c9663e6b0e3b7bf
Antonio Anderson Souza <antonioams@gmail.com>
2013-05-11 15:19:41 -0300

    reorganizado imports no Payment.java

--- src/br/com/hypersales/framework/model/register/Payment.java
+++ src/br/com/hypersales/framework/model/register/Payment.java
@@ -1,17 +1,5 @@
 package br.com.hypersales.framework.model.register;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPathExpressionException;
-
-import org.xml.sax.SAXException;
-
-import br.com.hypersales.framework.dao.Wsp;
-import br.com.hypersales.framework.dao.protheus.ARRAYOFSTRING;
-import br.com.hypersales.framework.dao.protheus.STRUCTRETURN;
 
 public class Payment {
 
