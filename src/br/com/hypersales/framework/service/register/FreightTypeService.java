bd082b84263d054ef4f247d2b8cabd9a9e70c24d
Everton Nascimento <evenascto@gmail.com>
2013-05-11 23:45:26 -0300

    FreightType fix

--- src/br/com/hypersales/framework/service/register/FreightTypeService.java
+++ src/br/com/hypersales/framework/service/register/FreightTypeService.java
@@ -9,8 +9,8 @@ public class FreightTypeService {
 	public JsonResultList<FreightType> getAllFreightTypes() {
 		JsonResultList<FreightType> result = new JsonResultList<FreightType>();
 		
-		result.add(new FreightType(br.com.hypersales.framework.util.enums.FreightType.C.getLabel(), br.com.hypersales.framework.util.enums.FreightType.C.getNome()));
-		result.add(new FreightType(br.com.hypersales.framework.util.enums.FreightType.F.getLabel(), br.com.hypersales.framework.util.enums.FreightType.F.getNome()));
+		result.add(new FreightType(br.com.hypersales.framework.util.enums.FreightType.C.getNome(), br.com.hypersales.framework.util.enums.FreightType.C.getLabel()));
+		result.add(new FreightType(br.com.hypersales.framework.util.enums.FreightType.F.getNome(), br.com.hypersales.framework.util.enums.FreightType.F.getLabel()));
 
 		result.setResponseId(RequestStatus.SUCCESS.statusCode());
 		result.setResponseMessage(RequestStatus.SUCCESS.toString());
