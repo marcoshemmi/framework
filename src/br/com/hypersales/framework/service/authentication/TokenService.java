a26622e679aa3f19febe6b6635546854fb9722bb
Everton Nascimento <evenascto@gmail.com>
2013-05-11 22:53:34 -0300

    autenticação de sellers

--- src/br/com/hypersales/framework/service/authentication/TokenService.java
+++ src/br/com/hypersales/framework/service/authentication/TokenService.java
@@ -1,9 +1,15 @@
 package br.com.hypersales.framework.service.authentication;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.UUID;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.xml.sax.SAXException;
 
 import br.com.hypersales.framework.dao.Wsp;
 import br.com.hypersales.framework.dao.protheus.ARRAYOFSTRING;
@@ -19,8 +25,15 @@ public class TokenService {
 	public JsonResult<Token> generateToken(String user, String password) {
 
 		JsonResult<Token> resultJson; 
+		User userFramework = null;
 		
-		User userFramework = authenticateUser(user, password);
+		//1: tenta autenticar como vendedor (Seller)
+		userFramework = authenticateSeller(user, password);
+		
+		//2: se não conseguir como vendedor, tenta autenticar como usuário Protheus
+		if(userFramework == null) {
+			userFramework = authenticateUser(user, password);
+		}
 		
 		if(userFramework == null) {
 			resultJson = new JsonResult<Token>(RequestStatus.UNAUTHORIZED_WRONG_AUTHENTICATION);
@@ -58,6 +71,69 @@ public class TokenService {
 		return resultJson;
 	}
 	
+
+    /**
+     * Autentica um usuário no Protheus.
+     * 
+     * <p>
+     * Aqui, retorna um usuário válido se o mesmo tiver sido criado no Protheus (ex: admin do protheus)
+     * <p>
+     * 
+     * 
+     */
+	private User authenticateSeller(String email, String password) {
+		Wsp daoWs = new Wsp();
+
+		User uRet = null;
+
+		try {
+			STRUCTRETURN retorno = daoWs.execute("Authentication_getUserByEmail", new String[]{email});
+			if (retorno.getRESPONSEMESSAGE().equals("OK")) {
+				ARRAYOFSTRING item = retorno.getSALESORDERID();
+				for(String record : item.getSTRING()) {
+					record = record.substring(1);
+					record = record.substring(0, record.length() -2);
+
+					String[] keyValueRecord = record.split("\",\"");
+
+					uRet = new User();
+
+					for(String kv : keyValueRecord) {
+						String[] keyValue = kv.split("\":\"");
+						if (keyValue[0].toUpperCase().equals("SELLERID")) {
+							uRet.setId(keyValue[1]);
+						}
+						if (keyValue[0].toUpperCase().equals("SELLERNAME")) {
+							uRet.setName(keyValue[1]);
+						}
+						if (keyValue[0].toUpperCase().equals("SELLERPASSWORD")) {
+							uRet.setPassword(keyValue[1]);
+						}
+					}
+					
+					if(! uRet.getPassword().equals(password)) {
+						//senha não confere
+						uRet = null;
+					}
+
+					break; //(apenas um registro)
+				}
+			}
+		} catch (XPathExpressionException | ParserConfigurationException
+				| SAXException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return uRet;
+	}
+
+    /**
+     * Autentica um Vendedor (seller).
+     * 
+     * <p>
+     * <p>
+     * 
+     */
 	private User authenticateUser(String user, String password) {
 		Wsp daoWs = new Wsp();
 
@@ -68,18 +144,13 @@ public class TokenService {
 			ARRAYOFSTRING item = retorno.getSALESORDERID();
 			for(String record : item.getSTRING()) {
 				uRet = new User();
-				//uRet.setId(record); 
-				/*
-				 * DESCOMENTAR ESSA LINHA QUANDO RESOLVER O PROBLEMA DOS IDS DA AUTENTICAÇÃO
-				 * 
-				 * 
-				 */
-				uRet.setId("000062");
+				uRet.setId(record); 
+				//uRet.setId("000062");
 				uRet.setName(user);
 				break; //(apenas um registro)
 			}
 		}
 		return uRet;
 	}
-
+	
 }
